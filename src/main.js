e702a49989b9b5ec4fd449ebc4507cf1aa3ef3ea
YunXinZhiCheng <867395636@qq.com>
2021-10-25 20:34:29 +0800

    安装element-ui 按需引入

--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// 按需引入element-ui
+import { Button, Select } from 'element-ui'
+Vue.use(Button)
+Vue.use(Select)
+
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
